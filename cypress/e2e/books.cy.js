ac8c34ffbd6084473843ca4efae25640caab4123
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-12-27 17:22:17 +0300

    Do the second and the third part of task

--- cypress/e2e/books.cy.js
+++ cypress/e2e/books.cy.js
@@ -5,39 +5,39 @@ describe("Books website testing", () => {
   });
 
   it("Log in with valid data", () => {
-    cy.login("bropet@mail.ru", "123");
+    cy.login("test@test.com", "test");
     cy.contains("Добро пожаловать ").should("be.visible");
   });
 
   it("Log in with empty email", () => {
-    cy.login(" ", "123");
+    cy.login(" ", "test");
     cy.get("#mail")
       .then(($el) => $el[0].checkValidity())
       .should("be.false");
   });
 
   it("Log in with empty password", () => {
-    cy.login("bropet@mail.ru", "");
+    cy.login("test@test.com", "");
     cy.get("#pass")
       .then(($el) => $el[0].checkValidity())
       .should("be.false");
   });
 
   it("Should add books", () => {
-    cy.login("bropet@mail.ru", "123");
+    cy.login("test@test.com", "test");
     cy.addBook("Сто лет одиночества", "Габриэль Гарсиа Маркес");
     cy.contains("Поющие в терновнике").should("be.visible");
   });
 
   it("Should add book to favourite", () => {
-    cy.login("bropet@mail.ru", "123");
+    cy.login("test@test.com", "test");
     cy.addBook("Унесенные ветром", "Маргарет Митчелл");
     cy.contains("Add to favorite").click();
     cy.contains("Delete from favorite").should("be.visible");
   });
 
   it("Download checking", () => {
-    cy.login("bropet@mail.ru", "123");
+    cy.login("test@test.com", "test");
     cy.addBook("1984", "Джодж Оруэлл");
     cy.contains("1984").click();
     cy.contains("Dowload book").should("be.visible");
